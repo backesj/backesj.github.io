d2baf99bee1132eeec798aad9507ccd2ab0600f8
LucasRowe <lucasrowepublic@gmail.com>
2014-05-09 01:43:04 -0400

    Changed font and moved header text. Added more css tweaks.

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,11 +1,13 @@
 //jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
+    if ($(".navbar").offset().top > 20) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
 });
+
+$('body').scrollspy({ offset: 70 });
 
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 /*$(function() {
