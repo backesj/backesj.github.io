ab4d3d901831d8e15f46426a8cefe7078be178c3
LucasRowe <lucasrowepublic@gmail.com>
2014-06-26 01:06:06 -0400

    Added RSVP section. Let the respondez si vous pli begin.

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -9,6 +9,36 @@ $(window).scroll(function() {
 
 $('body').scrollspy({ offset: 70 });
 
+var lgwidth = 1200;
+var mdwidth = 992;
+var smwidth = 768;
+
+$(document).ready (function() {
+    resize();
+    $(window).resize (function () {
+        resize();
+    });
+});
+
+function resize() {
+    var width = $(window).width();
+    var frame = $("#rsvpframe");
+    if (width >= lgwidth) {
+        frame.width(800);
+        frame.height(1025);
+    } else if (width >= mdwidth) {
+        frame.width(700);
+        frame.height(1025);
+    } else if (width >= smwidth) {
+        frame.width(600);
+        frame.height(1050);
+    } else {
+        // XS
+        frame.width(360);
+        frame.height(1275);
+    }
+}
+
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 /*$(function() {
     $('.page-scroll a').bind('click', function(event) {
